8f2cbca48a465fbcc8d60f4fdba534dcc5d0fa20
Stéphane <stephripa@gmail.com>
2023-08-20 12:39:16 +0200

    neural test

---
+++ neuralNetNumpy.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def neural_network(inputs, target, epochs, lr):
+    w1 = np.random.rand(2,4)
+    w2 = np.random.rand(4,1)
+
+    for epoch in range(epochs):
+        layer1 = sigmoid(np.dot(inputs, w1))
+        output = sigmoid(np.dot(layer1, w2))
+
+        error = target - output
+        delta2 = 2 * error * output * (1 - output)
+        delta1 = delta2.dot(w2.T) * layer1 * (1 - layer1)
+
+        w2 += lr * layer1.T.dot(delta2)
+        w1 += lr * inputs.T.dot(delta1)
+
+    return np.round(output.squeeze())
+
+inputs = np.array([[0,0], [0,1], [1,0], [1,1]])
+
+print("OR", neural_network(inputs, np.array([[0], [1], [1], [1]]), 5000, 0.1))
+
+
